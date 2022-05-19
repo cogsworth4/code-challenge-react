487f01526bca0d97a20f95b36267d6ee90989479
sbornia <sebastianbornia@gmail.com>
2022-05-19 15:42:33 -0300

    Initial code commit

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React code challenge</h1>
+      <div>
+        <p>
+          Place your <pre>WorkingHours</pre> component here!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
